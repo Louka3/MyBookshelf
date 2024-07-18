44650b81c44dea00f680bc58dcac01cee97ce5a4
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-07-17 23:01:06 -0400

    configuring jest for vite was not working. The best solution I found was to use vitest, which was designed to be used with vite. Very very similar to jest. The test for testing is working well

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
-const app = express();
+export const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-app.get("", (_req, res) => {
-  res.send("yes");
+app.post("/test", (_req, res) => {
+  res.sendStatus(200);
 });
-
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
---
+++ tests/serverTests/server.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { expect, test, describe } from "vitest";
+import { app } from "../../src/server/main";
+
+describe("POST from server", () => {
+  test("Testing if server testing works", async () => {
+    const response = await request(app).post("/test").send({
+      test: "data",
+    });
+    expect(response.statusCode).toBe(200);
+  });
+});
