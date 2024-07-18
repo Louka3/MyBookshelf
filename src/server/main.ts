512ce55f87f8fbfb79b798e77c6eeb86b42694ce
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-07-18 00:21:09 -0400

    Finished connection to mongoose db. Going to start writing tests for the book controllers and services

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,27 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const app = express();
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWD}@mybookshelf-cluster.oz0pux5.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(MONGO_URI);
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+db.on("error", (err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
+
+db.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
+});
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Vite + React + TypeScript!");
@@ -10,6 +30,6 @@ app.get("/hello", (_req, res) => {
 app.post("/test", (_req, res) => {
   res.sendStatus(200);
 });
-ViteExpress.listen(app, 3000, () =>
+ViteExpress.listen(app, process.env.PORT, () =>
   console.log("Server is listening on port 3000..."),
 );
